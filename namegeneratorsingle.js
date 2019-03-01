b2cd126a6556ec48a8c9474bcffacf54ef47445d
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2019-02-28 20:16:50 -0700

    Delete namegeneratorsingle.js

--- namegeneratorsingle.js
+++
@@ -1,6 +0,0 @@
-let namesSingle = ["Ampli", "Beyion", "Chegs", "Axon", "Saphire", "Sinx", "Zequila"]
-
-function newNameSingle() {
-    var randomNumber = Math.floor(Math.random() * (namesSingle.length));
-    document.getElementById('namessingle').innerHTML = namesSingle[randomNumber];
-}
